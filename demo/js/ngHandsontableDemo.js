337cc9c9a0fc8c7a79961b59d46a34fb6857b3da
Max Woo <maxithewoowoo@gmail.com>
2015-02-16 12:32:34 -0800

    isolated dynamic hottable

--- demo/js/ngHandsontableDemo.js
+++ demo/js/ngHandsontableDemo.js
@@ -44,4 +44,42 @@ angular.module('ngHandsontableDemo', ['ngHandsontable'])
 				}
 			);
 		}
+
+		$scope.db.dynamicColumns = [
+			{
+				data: 'id',
+				title: 'ID'},
+			{
+				data: 'name.first',
+				title: 'First Name',
+				readOnly: true
+			},
+			{
+				data: 'name.last',
+				title: 'Last Name',
+				readOnly: true
+			},
+			{data: 'address', title: 'Address', width: 150},
+			{data: 'product.description', type: 'autocomplete', title: 'Favorite food', width: 150, optionList: 'description in product.options'},
+			{data: 'price', title:'Price', type: 'numeric', width: 80, format: '$ 0,0.00'},
+			{data: 'isActive', type: 'checkbox', title: 'Is active', checkedTemplate: 'Yes', uncheckedTemplate: 'No', width:65}
+		];
+
+		setInterval(function () {
+			if( $scope.db.dynamicColumns[0].title == 'ID') {
+				$scope.db.dynamicColumns[3].readOnly = true;
+				$scope.db.dynamicColumns.shift();
+				$scope.afterChange = function () {
+//							console.log('afterChange: ','when ID column has been removed');
+				};
+
+			} else {
+				$scope.db.dynamicColumns[2].readOnly = false;
+				$scope.db.dynamicColumns.unshift({data: 'id', title: 'ID'});
+				$scope.afterChange = function () {
+//							console.log('afterChange: ','when ID column has been added');
+				};
+			}
+			$scope.$apply();
+		}, 3000);
 	});
